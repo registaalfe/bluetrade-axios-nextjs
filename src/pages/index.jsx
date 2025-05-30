de72b219a5da9e72c4e04e6c472f8d9ba5b96428
registaalfe <alfexcrstds07@gmail.com>
2025-05-30 22:56:11 +0700

    feat: uninstall chakraui bcs not support jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,20 @@
+import { Poppins } from 'next/font/google';
+
+
+// Set up Poppins with specified weights
+const poppinsFont = Poppins({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-poppins",
+});
+
+
 export default function Home() {
   return (
     <>
-
+      <div className={`${poppinsFont.variable} foreground`}>
+        <h1 className="text-3xl font-bold underline font-poppins">Ini Index</h1>
+      </div>
     </>
   );
 }
